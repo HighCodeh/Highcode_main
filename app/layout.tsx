c5dc9c2ef4da2f7d8a01567d9eaded5a6c3b98e1
Emanuel Magalhaes <emanuel.mag.jr@gmail.com>
2026-04-19 18:14:21 -0300

    clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -205,7 +205,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
     t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
     y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-})(window, document, "clarity", "script", "wea7xflruz");`}
+})(window, document, "clarity", "script", "wdbtwmc9x9");`}
         </Script>
         {/* End Microsoft Clarity */}
       </head>
